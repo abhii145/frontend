53780109078cd24efdf12b896fcfb6e336bc42fa
Abhishek Yadav <55094244+abhii145@users.noreply.github.com>
2024-06-17 23:51:04 +0530

    protecting routes based on role

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home, Cart, Auth, Search, Shipping, Orders, Dashboard, Products, Transaction, Customers, NewProducts, ProductManagement, TransactionManagement, Barcharts, Linecharts, Piecharts, Coupon } from "./pages";
-import { Suspense } from "react";
-import ProductDetails from "./components/ProductDetails";
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import Loader from "./components/Loader";
 import { useSelector } from "react-redux";
 import { RootState } from "./redux/store";
-import { AdminSidebar, Header } from "./components";
+import Loader from "./components/Loader";
+import {
+  Home,
+  Cart,
+  Auth,
+  Search,
+  Shipping,
+  Orders,
+  Dashboard,
+  Products,
+  Transaction,
+  Customers,
+  NewProducts,
+  ProductManagement,
+  TransactionManagement,
+  Barcharts,
+  Linecharts,
+  Piecharts,
+  Coupon,
+  AdminLayout,
+} from "./pages";
+import { AdminProtectedRoute, PrivateLayout, PublicLayout } from "./ProtectedRoutes";
+import ProductDetail from "./components/ProductDetails";
 
 const App = () => {
   const { user } = useSelector((state: RootState) => state.user);
@@ -15,38 +34,52 @@ const App = () => {
     <Router>
       <Toaster position="bottom-center" />
       <Suspense fallback={<Loader />}>
-        <Header user={user} />
         <Routes>
           {/* Public Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route element={<PublicLayout user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+          </Route>
 
-          {/* login Routes */}
+          {/* Auth Routes */}
           <Route path="/auth" element={<Auth />} />
 
-          {/* after login Routes */}
-          <Route path="/shipping" element={<Shipping />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/order/:id" element={<Orders />} />
+          {/* Private Routes */}
+          <Route element={<PrivateLayout />}>
+            <Route path="/shipping" element={<Shipping />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/order/:id" element={<Orders />} />
+          </Route>
 
-          {/* if user role is admin */}
-          <Route  element={<AdminSidebar/>} />
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/product" element={<Products />} />
-          <Route path="/admin/transactions" element={<Transaction />} />
-          <Route path="/admin/customers" element={<Customers />} />
-          <Route path="/admin/product/new" element={<NewProducts />} />
-          <Route path="/admin/product/:id" element={<ProductManagement />} />
-          <Route
-            path="/admin/transaction/:id"
-            element={<TransactionManagement />}
-          />
-          <Route path="/admin/chart/bar" element={<Barcharts />} />
-          <Route path="/admin/chart/line" element={<Linecharts />} />
-          <Route path="/admin/chart/pie" element={<Piecharts />} />
-          <Route path="/admin/app/coupon" element={<Coupon />} />
+          {/* Admin Routes */}
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route
+              element={
+                <AdminProtectedRoute
+                  isAuthenticated
+                  adminOnly
+                  admin={user?.role === "admin" ? true : false}
+                />
+              }
+            >
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="product" element={<Products />} />
+              <Route path="transactions" element={<Transaction />} />
+              <Route path="customers" element={<Customers />} />
+              <Route path="product/new" element={<NewProducts />} />
+              <Route path="product/:id" element={<ProductManagement />} />
+              <Route
+                path="transaction/:id"
+                element={<TransactionManagement />}
+              />
+              <Route path="chart/bar" element={<Barcharts />} />
+              <Route path="chart/line" element={<Linecharts />} />
+              <Route path="chart/pie" element={<Piecharts />} />
+              <Route path="app/coupon" element={<Coupon />} />
+            </Route>
+          </Route>
         </Routes>
       </Suspense>
     </Router>
